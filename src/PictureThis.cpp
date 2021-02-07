76b6c57daaeb14343f7d0f363d888e8b0a746d77
Fergus Waugh <fjswaugh@gmail.com>
2021-02-07 22:13:22 +0000

    Add sample image
    Load in a demo png and display it in within the `PictureThis` module.

--- src/PictureThis.cpp
+++ src/PictureThis.cpp
@@ -1,5 +1,7 @@
 #include "plugin.hpp"
-
+#include <nanovg.h>
+#include <stb_image.h>
+#include <widget/TransparentWidget.hpp>
 
 struct PictureThis : Module {
 	enum ParamIds {
@@ -27,6 +29,60 @@ struct PictureThis : Module {
 	}
 };
 
+struct PngWidget : TransparentWidget
+{
+    explicit PngWidget()
+    {}
+
+    void setImagePath(std::string new_path)
+    {
+        image_path_ = std::move(new_path);
+        new_image_path_ = true;
+
+        unsigned char* ptr = stbi_load(image_path_.c_str(), &width_, &height_, &comp_, 4);
+        image_data_.reset(ptr);
+    }
+
+    void draw(DrawArgs const& args) override
+    {
+        int const handle = nvgCreateImageRGBA(args.vg, width_, height_, 0, image_data_.get());
+
+        nvgBeginPath(args.vg);
+
+        auto const scale_factor_x = box.size.x / width_;
+        auto const scale_factor_y = box.size.y / height_;
+        auto const scale_factor = std::max(scale_factor_x, scale_factor_y);
+        nvgScale(args.vg, scale_factor, scale_factor);
+
+        NVGpaint const paint = nvgImagePattern(args.vg, x_, y_, width_, height_, 0.0f, handle, 1.0f);
+        nvgRect(args.vg, x_, y_, width_, height_);
+        nvgFillPaint(args.vg, paint);
+        nvgFill(args.vg);
+
+        nvgClosePath(args.vg);
+    }
+
+private:
+    struct Deleter
+    {
+        void operator()(unsigned char* ptr)
+        {
+            stbi_image_free(ptr);
+        }
+    };
+
+    using ImagePtr = std::unique_ptr<unsigned char, Deleter>;
+
+    std::string image_path_{};
+    bool new_image_path_{};
+
+    ImagePtr image_data_{};
+    int height_{};
+    int width_{};
+    int comp_{};
+    int x_{};
+    int y_{};
+};
 
 struct PictureThisWidget : ModuleWidget {
 	PictureThisWidget(PictureThis* module) {
@@ -46,8 +102,14 @@ struct PictureThisWidget : ModuleWidget {
 
 		// mm2px(Vec(116.851, 116.851))
 		addChild(createWidget<Widget>(mm2px(Vec(29.864, 5.502))));
-	}
-};
 
+        // Add a demo image
+        auto* image = new PngWidget();
+        image->box.pos = Vec{100.0f, 50.0f};
+        image->box.size = Vec{box.size.x - 150.0f, box.size.y - 100.0f};
+        image->setImagePath(asset::plugin(pluginInstance, "res/Test.png"));
+        addChild(image);
+    }
+};
 
 Model* modelPictureThis = createModel<PictureThis, PictureThisWidget>("PictureThis");
